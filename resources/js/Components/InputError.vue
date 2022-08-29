015b82101175640871b076aff1f37f9ce1098cab
eldynmohammad <eldyn.mohammad@gmail.com>
2022-08-29 16:31:21 +0700

    added models and controllers also showing error on input

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -1,10 +1,10 @@
 <script setup>
-defineProps(['message']);
+defineProps(["message"]);
 </script>
 
 <template>
     <div v-show="message">
-        <p class="text-sm text-red-600">
+        <p class="text-sm text-pink-600">
             {{ message }}
         </p>
     </div>
